e3c26fdc35c9d8565a55e9e579b6af926b1d4416
AhnYeonghoo <dksdudgn00@naver.com>
2023-07-22 16:42:25 +0900

    MFC OpenCV Setting

--- MFCApplication1Dlg.h
+++ MFCApplication1Dlg.h
@@ -36,57 +36,7 @@ public:
 	cv::Mat m_matImage; // 이미지 정보를 담고 있는 객체
 	BITMAPINFO* m_pBitmapInfo; // BItmap 정보를 담고 있는 구조체
 	
-	void createBitMapInfo(int w, int h, int bpp); // Bitmap 정보를 생성하는 함수
-	void drawImage(); // 그리기 작업을 수행하는 함수
-	
+
 };
 
-void CMFCApplication1Dlg::createBitMapInfo(int w, int h, int bpp)
-{
-	if (m_pBitmapInfo != nullptr)
-	{
-		delete m_pBitmapInfo;
-		m_pBitmapInfo = nullptr;
-	}
-	
-	if (bpp == 8)
-		m_pBitmapInfo = (BITMAPINFO*) new BYTE[sizeof(BITMAPINFO) + 255 * sizeof(RGBQUAD)];
-	else // 24 or 32bit
-		m_pBitmapInfo = (BITMAPINFO*) new BYTE[sizeof(BITMAPINFO)];
 
-	m_pBitmapInfo->bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-	m_pBitmapInfo->bmiHeader.biPlanes = 1;
-	m_pBitmapInfo->bmiHeader.biBitCount = bpp;
-	m_pBitmapInfo->bmiHeader.biCompression = BI_RGB;
-	m_pBitmapInfo->bmiHeader.biSizeImage = 0;
-	m_pBitmapInfo->bmiHeader.biXPelsPerMeter = 0;
-	m_pBitmapInfo->bmiHeader.biYPelsPerMeter = 0;
-	m_pBitmapInfo->bmiHeader.biClrUsed = 0;
-	m_pBitmapInfo->bmiHeader.biClrImportant = 0;
-
-	if (bpp == 8)
-	{
-		for (int i = 0; i < 256; i++)
-		{
-			m_pBitmapInfo->bmiColors[i].rgbBlue = (BYTE)i;
-			m_pBitmapInfo->bmiColors[i].rgbGreen = (BYTE)i;
-			m_pBitmapInfo->bmiColors[i].rgbRed = (BYTE)i;
-			m_pBitmapInfo->bmiColors[i].rgbReserved = 0;
-		}
-	}
-
-	m_pBitmapInfo->bmiHeader.biWidth = w;
-	m_pBitmapInfo->bmiHeader.biHeight = -h;
-}
-
-void CMFCApplication1Dlg::drawImage()
-{
-	CClientDC dc(GetDlgItem(IDC_PC_VIEW));
-
-
-	CRect rect;
-	GetDlgItem(IDC_PC_VIEW)->GetClientRect(&rect);
-
-	SetStretchBltMode(dc.GetSafeHdc(), COLORONCOLOR);
-	StretchDIBits(dc.GetSafeHdc(), 0, 0, rect.Width(), rect.Height(), 0, 0, m_matImage.cols, m_matImage.rows, m_matImage.data, m_pBitmapInfo, DIB_RGB_COLORS, SRCCOPY);
-}
